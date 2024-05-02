a34b6a21e5cebe2c7d272758257877c55d52e9af
svbelykh1 <b.s.v.belykh@gmail.com>
2024-05-03 01:42:09 +0300

    начали обработку формы

--- js/register.js
+++ js/register.js
@@ -1,1 +1,17 @@
-console.log("tst-register");
+//подписываемся на событие загрузки страницы
+window.addEventListener('load',()=>{
+    //ищем форму
+    let form = document.querySelector('form');
+    //подписываемся на событие submit передаем event в анонимную функцию хз для чего пока
+    form.addEventListener('submit',function(e){
+        //Ищем интпуты с паролями по id
+        let pwd = document.getElementById('pwd')
+        let cnfPwd = document.getElementById('cnfPwd')
+
+        //сравниваем значение и выводим alert
+        if( pwd.value != cnfPwd.value ){
+            alert("пароли не совпадают")
+        }
+    })
+    //console.log(form)
+})
